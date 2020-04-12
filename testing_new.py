0c96de9ccd10f9b054b50c7992d2f17df8713925
Your Name <you@example.com>
2020-04-12 11:08:32 +0700

    asd

---
+++ testing_new.py
@@ -0,0 +1,13 @@
+from RPIO import PWM
+
+servo = PWM.Servo()
+
+# Add servo pulse for GPIO 17 with 1200µs (1.2ms)
+servo.set_servo(17, 1200)
+
+# Add servo pulse for GPIO 17 with 2000µs (2.0ms)
+servo.set_servo(17, 2000)
+
+# Clear servo on GPIO17
+servo.stop_servo(17)
+
